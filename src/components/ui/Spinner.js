5cdf866bca51eac640721df5510eebee337f8936
9-Para <asmrahul17@gmail.com>
2020-08-04 14:34:06 +0530

    initial Commit

---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import spinner from "../../img/spinner.gif";
+function Spinner() {
+  return (
+    <img src={spinner} style={{width:"200px", margin:"auto", display:"block"}} alt="loading"/>
+  )
+}
+
+export default Spinner
